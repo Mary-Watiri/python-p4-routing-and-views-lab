da382f10cb589292f8fdd7be90871cdc1e6b5665
Mary Watiri <watirimary30@gmail.com>
2024-04-11 22:32:29 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -6,34 +6,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Python Operations with Flask Routing and Views</h1>"
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<param>')
-def print_string(param):
-    print(param) 
-    return param   
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
 
-@app.route('/count/<int:param>')
-def count(param):
-    numbers = '<br>'.join(str(num) for num in range(param))
-    return numbers
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
 
-@app.route('/math/<int:num1>/<operation>/<int:num2>')
-def math(num1, operation, num2):
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
     if operation == '+':
-        result = num1 + num2
+        return str(num1 + num2)
+    
     elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        result = num1 / num2
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return 'Invalid operation'
+        return str(num1 - num2)
 
-    return f'Result: {result}'
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Operation not recognized. Please use one of the following: + - * div %'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
